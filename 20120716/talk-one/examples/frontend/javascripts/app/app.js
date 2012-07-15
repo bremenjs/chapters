79e06df4e8de246c3639ee9424af75a908832785
André König <andre.koenig@gmail.com>
2012-07-15 15:34:09 +0200

    Created frontend structure.

---
+++ 20120716/talk-one/examples/frontend/javascripts/app/app.js
@@ -0,0 +1,27 @@
+/*!
+ * Bremen.js - Chapter #3
+ *
+ * CaloryCounter'
+ *
+ * Copyright(c) 2012 Bremen, Germany - All rights reserved.
+ *
+ * Authors:
+ *
+ *     Lukas Magedanz <lukas.magedanz@gmail.com>
+ *     André König <andre.koenig@gmail.com>
+ *
+ */
+define([
+	'backbone'
+],
+function (Backbone) {
+	function CaloryCounterApp($mainContainer) {
+		this.$main = $mainContainer;
+	};
+
+	CaloryCounterApp.prototype.run = function () {
+		// TODO: Start router
+	};
+
+	return CaloryCounterApp;
+});

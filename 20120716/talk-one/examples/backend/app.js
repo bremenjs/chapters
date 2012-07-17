49b48f5fcfdaef5e8bbc25623080566453892e4e
André König <andre.koenig@gmail.com>
2012-07-17 16:02:33 +0200

    Added Header.

--- 20120716/talk-one/examples/backend/app.js
+++ 20120716/talk-one/examples/backend/app.js
@@ -15,6 +15,7 @@ function crossdomain (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");﻿
 
     next();
 }
